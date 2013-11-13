e32023e81a281e9ccdbfeba3b27a2fbbe4e88ba8
Edward Gaba <egaba88@gmail.com>
2013-11-12 23:32:11 -0500

    create fixture adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,2 +1,20 @@
-var FixtureAdapter = DS.FixtureAdapter.extend();
+var FixtureAdapter = DS.FixtureAdapter.extend({
+  queryFixtures: function(fixtures, query, type) {
+    return fixtures.filter(function(fixture) {
+      for(var key in query) {
+        var val = null;
+        
+        if (!query.hasOwnProperty(key)) continue; 
+        
+        val = query[key];
+        
+        if (fixture[key] !== val) { 
+          return false; 
+        }
+      }
+      return true;
+    });
+  }
+});
+
 export default FixtureAdapter;
